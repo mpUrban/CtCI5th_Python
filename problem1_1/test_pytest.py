7cb6a92d5247794c7f13f12f1dc3339d866832c0
michael <urban22@gmail.com>
2019-03-17 20:01:46 -0400

    update testing

--- problem1_1/test_pytest.py
+++ problem1_1/test_pytest.py
@@ -1,12 +1,6 @@
 """
 Author: Michael Urban (c) 2019
 Email: echo ur3an221g5ail4com | tr 1345 @b.m
-
-Q: Implement an algorithm to determine if a string has all unique characters.
-What if you cannot use additional data structures?  
-
-Notes: Solved without data structures.
-
 """
 import problem1_1 as prob
 
